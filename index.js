ffded46617ccb99178664b755e1017eb4b075a4f
Nicolas Herrera <herreranicoo17@gmail.com>
2023-07-30 02:46:13 -0300

    Agregue modelo de paciente

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import path from "path";
+import "dotenv/config";
+import "./src/database/dbConnection";
 
-console.log("Server on port", 4000);
+// Puerto
+
+const app = express();
+
+app.set("port", process.env.PORT || 4000);
+app.listen(app.get("port"), () => {
+  console.log("Server on port", app.get("port"));
+});
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+console.log(__dirname);
+app.use(express.static(path.join(__dirname, "/public")));
+
+// Rutas
